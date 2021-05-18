3165e69fc9f349145d03cb844663e2a29462242b
jonashck1337 <jonashecke@gmail.com>
2021-05-18 21:47:16 +0200

    jo

---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+    import logo from "../../public/images/heart.svg";
+</script>
+
+<header>
+    <h1 class="bg-yellow-400">Healthspace</h1>
+    <img src={logo} </>
+</header>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,16 @@
 <script>
+  import Header from '../components/Header.svelte';
   export let name;
 </script>
 
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<Header />
+
 <main>
-  <h1>Welcome to SvelteKit123</h1>
+  <h1 >Welcome to SvelteKit123</h1>
   <h2 class="text-2xl bg-blue-500"> 
       Test for Website
   </h2> 
@@ -11,3 +18,4 @@
   <p class="bg-green-200 font-serif font-semibold">A bias neuron allows for more variations of weights to be stored. Biases add richer representation of the input space to the model’s weights. In the case of neural networks, a bias neuron is added to every layer. It plays a vital role by making it possible to move the activation function to the left or right on the graph.
     </p> 
 </main>
+
