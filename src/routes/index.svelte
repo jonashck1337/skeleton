3ae64394ffe4fcef6b5608a005f9e6519b4ad135
jonashck1337 <jonashecke@gmail.com>
2021-05-18 10:49:19 +0200

    demosvelte_update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<h2> 
+
+<h1>Welcome to SvelteKit123</h1>
+<h2 class="text-2xl bg-blue-500"> 
     Test for Website
 </h2> 
 <p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    export let name;
+  </script>
+  
+  <style>	
+    h1 {
+        class="text-2xl bg-blue-500";
+  }
+  </style>
+  
+  <h1>{name}</h1>
