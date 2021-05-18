c51eb0d234742219a03ecc05c4f5c3de0317b2ac
jonashck1337 <jonashecke@gmail.com>
2021-05-18 12:11:28 +0200

    yoyo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,10 @@
+<script>
+  export let name;
+</script>
+
 
 <h1>Welcome to SvelteKit123</h1>
 <h2 class="text-2xl bg-blue-500"> 
     Test for Website
 </h2> 
 <p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<script>
-    export let name;
-  </script>
-  
-  <style>	
-    h1 {
-        class="text-2xl bg-blue-500";
-  }
-  </style>
-  
-  <h1>{name}</h1>
